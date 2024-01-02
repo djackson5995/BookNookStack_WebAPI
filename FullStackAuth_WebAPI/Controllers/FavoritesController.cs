2466383d069c1406698ea08a8e5cae445c9d6fd0
djackson5995 <146674106+djackson5995@users.noreply.github.com>
2024-01-02 01:45:41 -0700

    created favorites controller and implemented a GET endpoint

---
+++ FullStackAuth_WebAPI/Controllers/FavoritesController.cs
@@ -0,0 +1,43 @@
+﻿using FullStackAuth_WebAPI.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoritesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoritesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("myFavorites"),Authorize]
+        public IActionResult GetFavorite()
+        {
+            try
+            {
+                string userId = User.FindFirstValue("id");
+
+                var favorites = _context.Favorite.Where(f => f.UserId.Equals(userId));
+                return StatusCode(200, favorites);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+
+
+            }
+
+            
+        }
+        
+
+    }
+

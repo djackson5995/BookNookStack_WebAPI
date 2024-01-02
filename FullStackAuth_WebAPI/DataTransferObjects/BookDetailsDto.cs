ac646bcd8c11000124c046a05898cd3cd3a7c4a9
djackson5995 <146674106+djackson5995@users.noreply.github.com>
2024-01-01 23:30:07 -0700

    made changeS to BookDetailDTO

--- FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
+++ FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
@@ -5,7 +5,9 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public UserForDisplayDto User { get; set; } 
+        public double Rating { get; set; }
+
+
 
     }
 }
